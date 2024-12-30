5846fccf9a47b0a92dad2f5d1b867c24b125938c
ousafa <ouaamou.safa@gmail.com>
2024-12-30 12:28:24 +0100

    exercice de liste générique est terminé

---
+++ listGénerique/Personne.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listGénerique
+{
+    internal class Personne
+    {
+        public String Code { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Age { get; set; }
+
+        // Constructeur par defaut
+        public Personne() {}
+        //constructeur parametrique
+        public Personne(String code ,String nom, String prenom, int age)
+        {
+            Code = code;
+            Nom = nom;
+            Prenom = prenom;
+            Age = age;
+        }
+
+        public String getinfo()
+        {
+            return " Je m’appelle " + Nom + " " + Prenom + " J’ai " + Age + " an j'ai le code " + Code;
+        }
+        // Creer une Liste des personnes
+        static List<Personne> personnes = new List<Personne>();
+        public void ajouter(Personne p)
+        {
+            personnes.Add( p );
+            Console.WriteLine("Personne ajoutée avec succès!\n");
+        }
+        public void modifier_nom(String code, string nom)
+        {
+            bool isUpdate = false;
+            foreach (Personne p in personnes)
+            {
+                if (code.Equals(p.Code))
+                {
+                    p.Nom = nom;
+                    Console.WriteLine($"Vous avez modifié le nom de la personne {code}");
+                    isUpdate = true;
+                    break;
+                }
+            }
+            if (!isUpdate)
+            {
+                Console.WriteLine($"Aucune personne trouvée avec le code {code}. Le nom n'a pas été modifié.");
+            }
+        }
+        public void supprimer(String nom)
+        {
+            bool isDelete = false;
+            foreach (Personne p in personnes)
+            {
+                if (p.Nom.Equals(nom))
+                {
+                    personnes.Remove(p);
+                    Console.WriteLine($"Vous avez supprimé la personne {Code}");
+                    isDelete = true;
+                    break;
+                }
+            }
+            if (!isDelete)
+            {
+                Console.WriteLine($"Aucune personne trouvée avec le nom {nom}. La suppression a échoué");
+            }
+        }
+
+        public void chercher(String code)
+        {
+            bool isFound = false;
+            foreach (Personne p in personnes)
+            {
+                if (p.Code.Equals(code))
+                {
+                    Console.WriteLine("la personne cherche est :" + p.getinfo());
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"Aucune personne trouvée avec le code {code}");
+            }
+        }
+        public void affichage()
+        {
+            foreach (Personne p in personnes)
+            {
+                  Console.WriteLine("Les personnes  :" + p.getinfo());
+            }
+        }
+        public int compter()
+        {
+            return personnes.Count;
+        }
+        public void categories()
+        {
+            foreach (Personne p in personnes)
+            {
+                if (p.Age <= 18)
+                    Console.WriteLine("la catégorie de " + p.Nom + "est: P" );
+                if (p.Age >= 19 && p.Age <= 35)
+                    Console.WriteLine("la catégorie de " + p.Nom + " est: J");
+                if (p.Age >= 36)
+                    Console.WriteLine("la catégorie de " + p.Nom + "est: G");
+            }
+        }
+    }
+}
---
+++ listGénerique/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listGénerique
+{
+    internal class Program
+    {
+        // MENU
+        public static void AfficherMenu()
+        {
+            Console.WriteLine("------------- Menu -----------");
+            Console.WriteLine("1. Ajouter une personne");
+            Console.WriteLine("2. Modifier une personne");
+            Console.WriteLine("3. supprimer une personne");
+            Console.WriteLine("4. Rechercher une personne");
+            Console.WriteLine("5. Afficher les Personnes");
+            Console.WriteLine("6. compter les Personnes");
+            Console.WriteLine("7. Afficher la gategorie");
+            Console.WriteLine("8. Quitter");
+        }
+        static void Main(string[] args)
+        {
+            String prenom,nom,code;
+            int age, choix;
+
+            // Creer un objet
+            Personne p = new Personne();
+
+
+            
+            do
+            {
+                AfficherMenu();
+                Console.Write("\nVeuillez choisir une option (1-8):");
+                choix = int.Parse(Console.ReadLine());
+                switch (choix)
+                {
+                    case 1:
+                        Console.Write("Entrez le code (format: P###):");
+                        code = Console.ReadLine();
+                        Console.Write("Entrez le prénom:");
+                        prenom = Console.ReadLine();
+                        Console.Write("Entrez le nom:");
+                        nom = Console.ReadLine();
+                        Console.Write("Entrez l'âge:");
+                        age = int.Parse(Console.ReadLine());
+                        p.ajouter(new Personne(code,nom,prenom,age));
+                        break;
+                    case 2:
+                        Console.Write("Entrez le code (format: P###):");
+                        code = Console.ReadLine();
+                        Console.Write("Entrez le nouveau nom:");
+                        nom = Console.ReadLine();
+                        p.modifier_nom(code, nom);
+                        break;
+                    case 3:
+                        Console.Write("Entrez le nom à supprimer:");
+                        nom = Console.ReadLine();
+                        p.supprimer(nom);
+                        break;
+                    case 4:
+                        Console.Write("Entrez le code à chercher:");
+                        code = Console.ReadLine();
+                        p.chercher(code);
+                        break;
+                    case 5:
+                        p.affichage();
+                        break;
+                    case 6:
+                        Console.WriteLine("le nombre des personnes est: " + p.compter());
+                        break;
+                    case 7:
+                        p.categories();
+                        break;
+                    case 8:
+                        Console.WriteLine("Au revoir!");
+                        break;
+                    default:
+                        Console.WriteLine("Option invalide. Veuillez choisir entre 1 et 8.");
+                        break;
+                }
+            } while(choix != 8);
+
+            Console.ReadKey();
+
+        }
+    }
+}
